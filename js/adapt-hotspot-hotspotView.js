032aa0ac8f30882629ab21cf6d279bd06c2bf543
moloko <mleathes+github@gmail.com>
2019-09-25 18:47:22 +0100

    trim spaces, double quotes > single update to proper our typical module definition style _.bind > Function.bind

--- js/adapt-hotspot-hotspotView.js
+++ js/adapt-hotspot-hotspotView.js
@@ -1,41 +1,39 @@
-define(function(require) {
+define([
+    'core/js/adapt',
+    'core/js/view/menuView',
+    './adapt-hotspot-hotspotItemView'
+], function(Adapt, MenuView, HotspotItemView) {
 
-    var Backbone = require('backbone');
-    var Adapt = require('coreJS/adapt');
-    var MenuView = require('coreViews/menuView');
-    var HotspotItemView = require("menu/adapt-hotspot-menu/js/adapt-hotspot-hotspotItemView");
-    
     var HotspotView = MenuView.extend({
         preRender: function() {
 
-            var nthChild = 0;
             this.model.getChildren().each(function(item) {
                 if(item.get('_isAvailable')) {
-                    var assessment = _.find(item.getChildren().toJSON(), function(it) { return typeof it._assessment !== "undefined"; } );
-                    if (assessment != undefined) {                    
+                    var assessment = _.find(item.getChildren().toJSON(), function(it) { return typeof it._assessment !== 'undefined'; } );
+                    if (assessment != undefined) {
                         var assessmentState = Adapt.assessment.get()[0].getState();
-                        item.set("_assessment", { 
-                                isComplete : assessmentState.isComplete,
-                                hasScore: true,
-                                scoreAsPercentage : assessmentState.scoreAsPercent,
-                                isPassed : assessmentState.isPass === true,
-                                isFailed : assessmentState.isPass === false
-                            });
+                        item.set('_assessment', {
+                            isComplete : assessmentState.isComplete,
+                            hasScore: true,
+                            scoreAsPercentage : assessmentState.scoreAsPercent,
+                            isPassed : assessmentState.isPass === true,
+                            isFailed : assessmentState.isPass === false
+                        });
                     }
-                    item.set("_locked", false);
-                    if (item.get("_lock")) {
-                        var contentObjects = item.get("_lock");
+                    item.set('_locked', false);
+                    if (item.get('_lock')) {
+                        var contentObjects = item.get('_lock');
                         var completeCount = 0;
-                        for( var i = 0; i < contentObjects.length; i++) if (Adapt.contentObjects.findWhere({_id:contentObjects[i]}).get("_isComplete")) completeCount++;
+                        for( var i = 0; i < contentObjects.length; i++) if (Adapt.contentObjects.findWhere({_id:contentObjects[i]}).get('_isComplete')) completeCount++;
                         if (completeCount < contentObjects.length) {
-                            item.set("_locked", true);
+                            item.set('_locked', true);
                         }
                     }
                 }
             });
 
             this.model.set({_areChildrenReady: false});
-            this.listenToOnce(this.model, "change:_areChildrenReady", this.onChildrenReady);
+            this.listenToOnce(this.model, 'change:_areChildrenReady', this.onChildrenReady);
 
             this.model.checkReadyStatus = function () {
                 // Filter children based upon whether they are available
@@ -51,22 +49,22 @@ define(function(require) {
         },
 
         onChildrenReady: function() {
-            if (this.$("img").length > 0) {
-                this.$el.imageready(_.bind(function() {
-                    this.setReadyStatus();
-                }, this));  
-            } else this.setReadyStatus();
+            if (this.$('img').length > 0) {
+                this.$el.imageready(this.setReadyStatus.bind(this));
+                return;
+            }
+            this.setReadyStatus();
         },
-        
+
         postRender: function() {
             var nthChild = 0;
             this.model.getChildren().each(function(item) {
                 if(item.get('_isAvailable')) {
                     nthChild ++;
-                    this.$('.hotspot-items').append(new HotspotItemView({model:item, nthChild:nthChild}).$el);
+                    this.$('.hotspot-items').append(new HotspotItemView({model: item, nthChild: nthChild}).$el);
                 }
             });
-            $(".menu").addClass("hotspot-menu");
+            $('.menu').addClass('hotspot-menu');
         }
 
     }, {
@@ -74,5 +72,5 @@ define(function(require) {
     });
 
     return HotspotView;
-    
+
 });
