ee5c44e8bbc83e54b176514d43e62f9393b3a56b
Matt Leathes <matt.leathes@kineo.com>
2020-12-16 11:26:05 +0000

    re-write the code that moves responsibility for setting _isReady (#13)
    * trim spaces, double quotes > single
    update to proper our typical module definition style
    _.bind > Function.bind
    
    * re-write the code that moves responsibility for setting _isReady from AdaptModel to here in such a way that it will actually run (fixes #12)

--- js/adapt-hotspot-hotspotView.js
+++ js/adapt-hotspot-hotspotView.js
@@ -1,72 +1,72 @@
-define(function(require) {
+define([
+    'core/js/adapt',
+    'core/js/view/menuView',
+    './adapt-hotspot-hotspotItemView'
+], function(Adapt, MenuView, HotspotItemView) {
 
-    var Backbone = require('backbone');
-    var Adapt = require('coreJS/adapt');
-    var MenuView = require('coreViews/menuView');
-    var HotspotItemView = require("menu/adapt-hotspot-menu/js/adapt-hotspot-hotspotItemView");
-    
     var HotspotView = MenuView.extend({
-        preRender: function() {
 
-            var nthChild = 0;
+        preRender: function() {
             this.model.getChildren().each(function(item) {
                 if(item.get('_isAvailable')) {
-                    var assessment = _.find(item.getChildren().toJSON(), function(it) { return typeof it._assessment !== "undefined"; } );
-                    if (assessment != undefined) {                    
+                    var assessment = _.find(item.getChildren().toJSON(), function(it) { return typeof it._assessment !== 'undefined'; } );
+                    if (assessment != undefined) {
                         var assessmentState = Adapt.assessment.get()[0].getState();
-                        item.set("_assessment", { 
-                                isComplete : assessmentState.isComplete,
-                                hasScore: true,
-                                scoreAsPercentage : assessmentState.scoreAsPercent,
-                                isPassed : assessmentState.isPass === true,
-                                isFailed : assessmentState.isPass === false
-                            });
+                        item.set('_assessment', {
+                            isComplete : assessmentState.isComplete,
+                            hasScore: true,
+                            scoreAsPercentage : assessmentState.scoreAsPercent,
+                            isPassed : assessmentState.isPass === true,
+                            isFailed : assessmentState.isPass === false
+                        });
                     }
-                    item.set("_locked", false);
-                    if (item.get("_lock")) {
-                        var contentObjects = item.get("_lock");
+                    item.set('_locked', false);
+                    if (item.get('_lock')) {
+                        var contentObjects = item.get('_lock');
                         var completeCount = 0;
-                        for( var i = 0; i < contentObjects.length; i++) if (Adapt.contentObjects.findWhere({_id:contentObjects[i]}).get("_isComplete")) completeCount++;
+                        for( var i = 0; i < contentObjects.length; i++) if (Adapt.contentObjects.findWhere({_id:contentObjects[i]}).get('_isComplete')) completeCount++;
                         if (completeCount < contentObjects.length) {
-                            item.set("_locked", true);
+                            item.set('_locked', true);
                         }
                     }
                 }
             });
 
-            this.model.set({_areChildrenReady: false});
-            this.listenToOnce(this.model, "change:_areChildrenReady", this.onChildrenReady);
-
-            this.model.checkReadyStatus = function () {
-                // Filter children based upon whether they are available
-                var availableChildren = new Backbone.Collection(this.getChildren().where({_isAvailable: true}));
-                // Check if any return _isReady:false
-                // If not - set this model to _isReady: true
-                if (availableChildren.findWhere({_isReady: false})) return;
-
-                this.set({_areChildrenReady: true});
-            };
+            /*
+            * stop Adapt.Model from setting _isReady on the model as soon as child models are ready
+            * instead, we'll handle doing that so that we can make it wait for any menu images to load
+            */
+            this.model.stopListening(Adapt[this.model._children], "change:_isReady");
+            this.model.set('_areChildrenReady', false);
+            this.listenTo(Adapt[this.model._children], "change:_isReady", this.checkReadyStatusOfChildren);
+            this.listenToOnce(this.model, 'change:_areChildrenReady', this.onChildrenReady);
 
             MenuView.prototype.preRender.call(this);
         },
 
-        onChildrenReady: function() {
-            if (this.$("img").length > 0) {
-                this.$el.imageready(_.bind(function() {
-                    this.setReadyStatus();
-                }, this));  
-            } else this.setReadyStatus();
+        checkReadyStatusOfChildren: function() {
+            if (this.model.getAvailableChildren().findWhere({_isReady: false})) return;
+
+            this.model.set('_areChildrenReady', true);
         },
-        
+
+        onChildrenReady: function() {
+            if (this.$('img').length > 0) {
+                this.$el.imageready(this.setReadyStatus.bind(this));
+                return;
+            }
+            this.setReadyStatus();
+        },
+
         postRender: function() {
             var nthChild = 0;
             this.model.getChildren().each(function(item) {
                 if(item.get('_isAvailable')) {
                     nthChild ++;
-                    this.$('.hotspot-items').append(new HotspotItemView({model:item, nthChild:nthChild}).$el);
+                    this.$('.hotspot-items').append(new HotspotItemView({model: item, nthChild: nthChild}).$el);
                 }
             });
-            $(".menu").addClass("hotspot-menu");
+            $('.menu').addClass('hotspot-menu');
         }
 
     }, {
@@ -74,5 +74,5 @@ define(function(require) {
     });
 
     return HotspotView;
-    
+
 });
